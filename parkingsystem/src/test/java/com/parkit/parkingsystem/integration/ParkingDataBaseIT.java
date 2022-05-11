cfcf97ed1aa4a20e4cf914bf930ef959df09bcf3
Ricama <steven.mahouasse1999@gmail.com>
2022-05-11 13:52:56 +0200

    Test ParkingDataBaseIT

--- parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,16 +53,28 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+        Ticket ticket = null;
+        int parkingSpot = 0;
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+        ticket = ticketDAO.getTicket("ABCDEF");
+        parkingSpot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+        assertNotNull(ticket);
+        assertEquals(2,parkingSpot);
     }
 
     @Test
     public void testParkingLotExit() throws InterruptedException {
+        Ticket ticket = null;
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         Thread.sleep(500);
         parkingService.processExitingVehicle();
+        ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket.getOutTime());
+        assertEquals(0,ticket.getPrice());
+
     }
 
 }
